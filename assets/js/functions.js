266775c2a9e3d0dd4cb365842c5d3d0035b49522
Simone <dragodiandrus@live.it>
2017-06-04 23:01:46 +0200

    Update functions.js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -56,7 +56,7 @@ $( document ).ready(function() {
     var curActive = $('.side-nav').find('.is-active'),
         curPos = $('.side-nav').children().index(curActive),
         lastItem = $('.side-nav').children().length - 1,
-        nextPos = $('.side-nav').children().length - 2;
+        nextPos = curPos + 1;
 
     updateNavs(lastItem);
     updateContent(curPos, nextPos, lastItem);
